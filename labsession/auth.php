3f4388a21cdcfb434ded0341f1cb375508d29c99
kirillsankov <73188231+kirillsankov@users.noreply.github.com>
2021-12-27 00:59:18 +0300

    Add files via upload

---
+++ labsession/auth.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+    if(!isset($_SESSION["login"]) && !isset($_SESSION["password"])) {
+        header("Location: /labsession/");
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <h1>
+            <?
+            echo("Привет, " . $_SESSION["login"]);
+            ?>
+        </h1>
+        <a class="exit-btn" href="index.php?logout=yes">Выйти</a>
+    </div>
+</body>
+</html>
